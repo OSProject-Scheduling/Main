e4e08be6c60abcc92c180d46a5a21327741d5a62
jhcard <indo7872@naver.com>
2022-05-06 21:41:29 +0900

    알고리즘 간결화

--- Scheduling/SRTN.java
+++ Scheduling/SRTN.java
@@ -17,49 +17,71 @@ public class SRTN extends Algorithm{
 	void schedulling() {
 		CalculateTime();
 		
-		if(!AlgorithmList.isEmpty() && time == AlgorithmList.peekFirst().ArrivalTime) {
-			ReadyQueue.add(AlgorithmList.poll()); 	// AlgorithmList에서 ReadyQueue로 이동(ArrivalTime에 맞으면)
-			manager.ReadyQueue.create_form(ReadyQueue);
-		}
-		if (PresentProcess != null) {																				// 현재 FCFS가 있으며
-			if (ReadyQueue.size() > 1) {																		// Ready큐에 1개 이상 들어있다면
-				for (int i = ReadyQueue.size() - 2; i >= 0; i--) {												// 맨 뒤에 프로세스를 하나씩 비교를 하며 정렬
-					if (ReadyQueue.get(ReadyQueue.size() - pass).BurstTime < ReadyQueue.get(i).BurstTime) {		// PASS의 용도는 만약 바뀐다면 바꾼 인덱스값도 증가가 되므로 그거 맞춰주기 위해 사용
-						tmpProcess = ReadyQueue.get(ReadyQueue.size() - pass);
-						ReadyQueue.set(ReadyQueue.size() - pass, ReadyQueue.get(i));
-						ReadyQueue.set(i, tmpProcess);
-						pass++;
-					}
+		/*--------------------------종료 조건---------------------------*/
+		Terminate();
+		
+		/*------------------------Ready Queue------------------------*/
+		while (!AlgorithmList.isEmpty() && time == AlgorithmList.peekFirst().ArrivalTime) {
+			for (int i = 0; i <= ReadyQueue.size(); i++) {			// 레디큐의 모든 프로세스를 검사
+				if (i == ReadyQueue.size()) {						// i가 레디큐의 마지막 인자까지 검사했으면 레디큐 맨 뒤에 프로세스 추가
+					ReadyQueue.add(AlgorithmList.poll());
+					manager.ReadyQueue.create_form(ReadyQueue);
+					break;
 				}
-				manager.ReadyQueue.create_form(ReadyQueue);
-				pass = 1;
-			}
-		}
-		if(PresentProcess.BurstTime > ReadyQueue.get(0).BurstTime) {
-			ReadyQueue.add(PresentProcess);
-			PresentProcess = ReadyQueue.poll();
-		}
-		if(PresentProcess != null && !ReadyQueue.isEmpty())														// 현재 FCFS가 있으며, Ready큐에 하나라도 있다면
-		{
-			if(PresentProcess.BurstTime > ReadyQueue.get(0).BurstTime) 											// 현재 진행중인 것과 Ready큐에 있는 BurstTime이 가장 짦은 것과 비교
-			{
-				ReadyQueue.add(PresentProcess);
-				PresentProcess = ReadyQueue.poll();
-				manager.ReadyQueue.create_form(ReadyQueue);
+
+				if (ReadyQueue.get(i).BurstTime > AlgorithmList.peekFirst().BurstTime) {	// 프로세스 리스트의 맨 앞 프로세스의 BT가 i번째 프로세스의 BT보다 짧으면
+					ReadyQueue.add(i, AlgorithmList.poll());								// 레디큐의 i번째에 프로세스 추가(새치기)
+					manager.ReadyQueue.create_form(ReadyQueue);
+					break;
+				}
 			}
 		}
 		
-		if (PresentProcess == null) { // 현재 진행중이 FCFS가 끝났고
-			if (!ReadyQueue.isEmpty()) { // ReadyQueue가 비어있지 않으면
-				PresentProcess = ReadyQueue.poll(); // ReadyQueue에서 BurstTime이 가장 짦은 프로세스를 진행시킴
-				manager.ReadyQueue.create_form(ReadyQueue);
+		/*------------------------SRTN 알고리즘------------------------*/
+		for(int i=0; i<CoreCount; i++) {
+			if(PresentProcess[i] != null && !ReadyQueue.isEmpty())					// 현재 실행 중인 프로세스가 있고, Ready큐에 하나라도 있다면
+			{
+				if(PresentProcess[i].BurstTime > ReadyQueue.get(0).BurstTime) 		// 현재 진행중인 프로세스의 BT와 레디큐 맨 앞 프로세스의 BT 비교해서 레디큐의 BT가 더 작으면 선점 당하므로
+				{
+					for (int j = 0; j <= ReadyQueue.size(); j++) {				// 레디큐의 모든 프로세스를 검사
+						if (i == ReadyQueue.size()) {							// i가 레디큐의 마지막 인자까지 검사했으면 레디큐 맨 뒤에 현재 프로세스 추가
+							ReadyQueue.add(PresentProcess[i]);
+							break;
+						}
+
+						if (ReadyQueue.get(j).BurstTime > PresentProcess[i].BurstTime) {	// 프로세스 리스트의 맨 앞 프로세스의 BT가 i번째 프로세스의 BT보다 짧으면
+							ReadyQueue.add(j, PresentProcess[i]);							// 레디큐의 i번째에 현재 프로세스 추가(새치기)
+							break;
+						}
+					}
+
+					PresentProcess[i] = ReadyQueue.poll();								// 레디큐의 맨 앞이 선점함
+				}
 			}
 		}
-		if(PresentProcess == null && ReadyQueue.isEmpty() && AlgorithmList.isEmpty()) {
-			manager.GhanttChart_1.addLastSecond();
-			return;
+		manager.ReadyQueue.create_form(ReadyQueue);
+		
+		for(int i=0; i<CoreCount; i++) {
+			if (PresentProcess[i] == null) { 									// 현재 진행중인 프로세스가 없고
+				if (!ReadyQueue.isEmpty()) { 								// ReadyQueue가 비어있지 않으면
+					PresentProcess[i] = ReadyQueue.poll(); 					// ReadyQueue에서 BurstTime이 가장 짦은 프로세스를 진행시킴
+				}
+				else {														// 레디큐가 비어있으면 대기전력 0.1+
+					elec += 0.1;
+				}
+			}
 		}
-		GUIELEC();																			// GhanttChart 표시
-		if(!(PresentProcess == null)) PresentProcess.BurstTime -= CoreWork; // 현재 FCFS가 비어있지 않으면 bursttime에서 처리량 빼주기
+		manager.ReadyQueue.create_form(ReadyQueue);
+		
+		GUISetting();
+		for(int i=0; i<CoreCount; i++) {
+			if(!(PresentProcess == null)) {
+		    	PresentProcess[i].BurstTime -= CoreWork[i];	// 현재 실행 중인 프로세스가 있다면 Bursttime에서 처리량 빼주기
+		    	if(CoreWork[i] == 1)						// e코어이면 전력+1
+		    		elec += 1;
+		    	else									// p코어이면 전력+3
+		    		elec += 3;
+		    }
+		}
 	}
 }
